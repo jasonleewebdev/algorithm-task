bc3f70ed5d0b6e5dbe352635ede1c32ab072d39e
jasonleewebdev <jason.lee.webdev@gmail.com>
2023-03-12 22:16:12 +0900

    upload practice01_02

---
+++ do_it_algorithm/src/chap01/practice01_02/Max3Method.java
@@ -0,0 +1,22 @@
+package chap01.practice01_02;
+
+public class Max3Method {
+
+	static int max3(int a, int b, int c) {
+		int max = a;
+		
+		if(max < b) {
+			max = b;
+		}
+		
+		if(max < c) {
+			max = c;
+		}
+		
+		return max;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("max(3, 2 ,1) = " + max3(3, 2, 1));
+	}
+}
